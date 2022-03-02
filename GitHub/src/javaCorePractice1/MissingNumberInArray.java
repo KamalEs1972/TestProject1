2d6ba37f54dbdbd267945f0e97afac31e32499ab
=Kamal <=>
2022-03-02 13:01:47 -0500

    second Commit

---
+++ GitHub/src/javaCorePractice1/MissingNumberInArray.java
@@ -0,0 +1,17 @@
+package javaCorePractice1;
+
+public class MissingNumberInArray {
+
+	public static void main(String[] args) {
+		// n=6;
+		//sum=(n*(n+1))/2=21
+		int [] arr= {3,1,5,7,4,2};
+		int n=arr.length+1;
+		int sum=(n*(n+1))/2;
+		for(int i=0;i<arr.length;i++) {
+			sum=sum-arr[i];
+		}
+			System.out.println("The missing number is " +sum);
+	}
+
+}
